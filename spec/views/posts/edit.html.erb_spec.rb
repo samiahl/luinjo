9a6ea8109e78232c011c9f192613bc025718dc57
Ilkka Hakkarainen <ilkka.hakkarainen@cs.helsinki.fi>
2014-03-21 10:24:12 +0200

    Create Post model by scaffolding
    Resolves #1

---
+++ spec/views/posts/edit.html.erb_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe "posts/edit" do
+  before(:each) do
+    @post = assign(:post, stub_model(Post,
+      :title => "MyString",
+      :description => "MyText",
+      :url => "MyString"
+    ))
+  end
+
+  it "renders the edit post form" do
+    render
+
+    # Run the generator again with the --webrat flag if you want to use webrat matchers
+    assert_select "form[action=?][method=?]", post_path(@post), "post" do
+      assert_select "input#post_title[name=?]", "post[title]"
+      assert_select "textarea#post_description[name=?]", "post[description]"
+      assert_select "input#post_url[name=?]", "post[url]"
+    end
+  end
+end
