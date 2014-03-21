96f74ed56365df98c7734bfc21f47d7f2652a90c
Ilkka Hakkarainen <ilkka.hakkarainen@cs.helsinki.fi>
2014-03-21 14:29:37 +0200

    Connect Posts to Users
    Each Post has a single submitter.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
+
+  belongs_to :user
   belongs_to :information_cone
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140321121159) do
+ActiveRecord::Schema.define(version: 20140321122909) do
 
   create_table "information_cones", force: true do |t|
     t.string   "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20140321121159) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "information_cone_id"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
