501cab5f228ab7e0ce866dac381fc8ddc95134af
Ilkka Hakkarainen <ilkka.hakkarainen@cs.helsinki.fi>
2014-04-04 18:12:26 +0300

    Generate friendlier Information Cone titles

--- spec/fabricators/information_cone_fabricator.rb
+++ spec/fabricators/information_cone_fabricator.rb
@@ -1,4 +1,8 @@
 Fabricator(:information_cone) do
-  title { Faker::Lorem.word }
+  title { camelcase(Faker::Company.catch_phrase).truncate 32 }
   description { Faker::Lorem.paragraph }
+end
+
+def camelcase(str)
+  str.downcase.split.each_with_index.map { |v,i|  i == 0 ? v : v.capitalize }.join  
 end
